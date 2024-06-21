714528ff3cc81b1404d2189f720058f46a735f27
mazda01 <khatoonmazda94@email.com>
2024-06-21 14:34:55 +0530

    age

---
+++ age.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int age;
+
+    printf("please Enter your age \n");
+    scanf("%d",&age);
+
+    printf("my age is: %d \n",age);
+
+    if(age>80)
+    {
+        printf("I am the boss");
+
+    }
+    
+    
+        
+
+    
+
+    
+}
